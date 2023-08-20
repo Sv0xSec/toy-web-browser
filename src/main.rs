50d8c3d1be59b37bd3d4ef148e8715011e58e392
Sv0xSec <sv0xsec@gmail.com>
2023-08-20 12:44:42 +0200

    Request function connects, write GET request and read server response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use crate::url::URL;
+
 pub mod url;
 
 fn main() {
-    println!("Hello, world!");
+    let url = URL::new("http://example.org:80/index.html").unwrap();
+    println!("{:?}", url);
+
+    let body = url.request();
+    show(body);
+}
+
+fn show(body: String) -> () {
+    let mut in_angle = false;
+    for c in body.chars() {
+        match c {
+            '<' => in_angle = true,
+            '>' => in_angle = false,
+            _ if in_angle == false => print!("{c}"),
+            _ => (),
+        }
+    }
 }
--- src/url.rs
+++ src/url.rs
@@ -1,4 +1,7 @@
 use std::fmt;
+use std::net::{TcpStream, ToSocketAddrs};
+use std::io::{Write, Read};
+use httparse::{Response, EMPTY_HEADER};
 
 #[derive(Debug)]
 pub struct URL<'a> {
@@ -9,8 +12,10 @@ pub struct URL<'a> {
 
 impl URL<'_> {
     pub fn new(url: &str) -> Result<URL, UrlError> {
+        // Check '://' pattern is present once in url and split the latter or raise an error
         let (url_scheme, url_without_scheme) = if url.contains("://") {
             let url_collection = url.split("://").collect::<Vec<&str>>();
+            // Check if pattern appears multiple time
             if url_collection.len() == 2 {
                 (url_collection[0], url_collection[1])
             } else {
@@ -20,6 +25,7 @@ impl URL<'_> {
             return Err(UrlError { message: String::from("'://' not detected") });
         };
 
+        //Get the host part of the url and its length
         let (url_host, host_length) = if url_without_scheme.contains("/") {
             let h = url_without_scheme.split("/").collect::<Vec<&str>>()[0];
             (h, h.len())
@@ -27,6 +33,7 @@ impl URL<'_> {
             (url_without_scheme, url_without_scheme.len())
         };
 
+        //Get the path part of the url
         let url_path = &url_without_scheme[host_length..];
 
         Ok(URL{
@@ -34,6 +41,38 @@ impl URL<'_> {
             host: url_host,
             path: url_path,
         })
+    }
+
+    pub fn request(&self) -> String {
+        //let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP)).unwrap();
+        //TO-DO : Error if no port, even 80
+        //let address : SockAddr = if let Some(socket_address) = self.host.to_socket_addrs().unwrap().next() {
+        //    socket_address.into()
+        //} else {
+        //    panic!("Hostname could not be resolved");
+        //};
+        //println!("{:?}", address);
+        //socket.connect(&address).unwrap();
+        //socket.send(String::from(format!("GET {} HTTP/1.0\r\nHost: {}\r\n\r\n", self.path, self.host)).as_bytes()).unwrap();
+        //let mut response = [MaybeUninit::uninit(); 1024];
+        //let response_length = socket.recv(&mut response).unwrap();
+        //let transmuted = unsafe { transmute::<_, [u8; 1024]>(response) };
+        //let data = &transmuted[0..response_length];
+        //println!("{:?}",data);
+
+        let mut stream = TcpStream::connect(self.host).unwrap();
+
+        let _ = stream.write(String::from(format!("GET {} HTTP/1.0\r\nHost: {}\r\n\r\n", self.path, self.host)).as_bytes());
+        let mut raw_response: [u8; 4096] = [0; 4096];
+        let resp_length= stream.read(&mut raw_response).unwrap();
+        let data = std::str::from_utf8(&raw_response[0..resp_length]).unwrap();
+
+        let collection = data.split("\r\n\r\n").collect::<Vec<&str>>();
+        if collection.len() == 2 {
+            String::from(collection[1])
+        } else {
+            panic!("Multiple '\r\n\r\n' pattern");
+        }
     }
 }
 
